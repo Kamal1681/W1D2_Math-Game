8d6baecea14dd35ad24f42738fe0bec72c5329e6
Kamal Maged <Kamal_Maged@yahoo.com>
2019-01-08 12:04:06 -0500

    Initial Commit

---
+++ W1D2_Maths/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  W1D2_Maths
+//
+//  Created by Kamal Maged on 2019-01-08.
+//  Copyright © 2019 Kamal Maged. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
